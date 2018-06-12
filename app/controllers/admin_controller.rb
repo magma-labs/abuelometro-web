1a85e42dc60a95c242e724bc7d4ac634207219f3
Hector Rodriguez <hector.m.rm@hotmail.com>
2018-06-12 13:26:39 -0500

    [Am-23] Create admin login page - create admin route - create admin view

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# To blkalbala
+class AdminController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-
   resources :profiles
-
   root to: 'home#index'
-
+  resources :admin
 end
