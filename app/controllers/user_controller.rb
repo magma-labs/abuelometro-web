2c65a35e456ea7ec70f8ebc0673583704d27214b
Saul Rubio <rubiocanino98@gmail.com>
2018-06-13 10:06:20 -0500

    Merge branch 'master' into feature/login-hompage-view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,11 +2,15 @@
 
 # To control thw workflow in the user
 class UserController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
 
   def index
     @elders = Elder.all
     render template: 'users/index'
 
+  end
+
+  def caretaker
+    render template: 'users/form-takecare'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 Rails.application.routes.draw do
   mount API::Base, at: '/'
   resources :elders
@@ -6,8 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   devise_scope :user do
-   get "/admin" => "user#index"
-   delete "/users/sign_out" => "devise/sessions#destroy"
-   get '/admin/index' => 'devise/index'
+    get '/admin' => 'user#index'
+    delete '/users/sign_out' => 'devise/sessions#destroy'
+    get '/addTakecare' => 'user#caretaker'
   end
 end
