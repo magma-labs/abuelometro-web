8e8eae8c4013a76cddf111e17578576d5e675019
Hector Rodriguez <hector.m.rm@hotmail.com>
2018-06-13 00:52:01 -0500

    test-heroku

--- db/migrate/20180613034115_rename_columns_in_user.rb
+++
@@ -1,8 +0,0 @@
-class RenameColumnsInUser < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :user_name, :name
-    rename_column :users, :user_first_name, :first_name
-    rename_column :users, :user_last_name, :second_name
-    rename_column :users, :user_phone, :phone
-  end
-end
