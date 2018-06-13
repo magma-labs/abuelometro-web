7a90fee4e906437c0674045f107aba6743f40377
Saul Rubio <rubiocanino98@gmail.com>
2018-06-13 01:06:25 -0500

    [AM-24] Create the admin home page
    It was created the landig home for the admins in order to
    have a control of all the users.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'home#index'
-
+  devise_scope :user do
+   get "/admin" => "user#index"
+   delete "/users/sign_out" => "devise/sessions#destroy"
+   get '/admin/index' => 'devise/index'
+  end
 end
